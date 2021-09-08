c723cdffde620ea909feedaa54c730aaac72b548
Garcez17 <ggarcez613@gmail.com>
2021-09-08 17:42:25 -0300

    Create user admin feat

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+
+import { IHashProvider } from '@shared/container/providers/hashProvider/models/IHashProvider';
+import { IUsersRepository } from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+export class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email already used.');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
