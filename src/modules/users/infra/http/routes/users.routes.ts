20e4c554003754a6033c574b7d1ce1c139ec1a7a
Garcez17 <ggarcez613@gmail.com>
2021-09-10 20:52:21 -0300

    User Session

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import { UsersController } from '../controllers/UsersController';
+import { sessionsRouter } from './sessions.routes';
 
 const usersController = new UsersController();
 
 const usersRouter = Router();
 
-usersRouter.post('/', usersController.create);
+usersRouter.use('/session', sessionsRouter);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 export { usersRouter };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'reflect-metadata';
 import 'express-async-errors';
+import { errors } from 'celebrate';
+import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
+
+import { routes } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server started on port 3333!'));
